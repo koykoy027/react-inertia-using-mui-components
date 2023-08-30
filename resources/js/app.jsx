a3604114b3fde3e16d7b1da6ac0af63a1d8ea119
Jhunriz <93062821+Jhunriz@users.noreply.github.com>
2023-08-30 09:15:04 +0800

    avatar-icon-add

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,36 +1,40 @@
-import './bootstrap';
-import '../css/app.css';
+import "./bootstrap";
+import "../css/app.css";
 
-import { createRoot } from 'react-dom/client';
-import { createInertiaApp } from '@inertiajs/react';
-import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
-import { createTheme } from '@mui/material';
-import { ThemeProvider } from '@emotion/react';
-import { blue } from '@mui/material/colors';
+import { createRoot } from "react-dom/client";
+import { createInertiaApp } from "@inertiajs/react";
+import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers";
+import { createTheme } from "@mui/material";
+import { ThemeProvider } from "@emotion/react";
+import { blue } from "@mui/material/colors";
 
-const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+const appName = import.meta.env.VITE_APP_NAME || "Laravel";
 
-const theme = createTheme ({
+const theme = createTheme({
     palette: {
         primary: {
-            main: blue[500]
-        }
-    }
+            main: blue[500],
+        },
+    },
 });
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
-    resolve: (name) => resolvePageComponent(`./Pages/${name}.jsx`, import.meta.glob('./Pages/**/*.jsx')),
+    resolve: (name) =>
+        resolvePageComponent(
+            `./Pages/${name}.jsx`,
+            import.meta.glob("./Pages/**/*.jsx")
+        ),
     setup({ el, App, props }) {
         const root = createRoot(el);
 
         root.render(
-        <ThemeProvider theme={theme}>
+            <ThemeProvider theme={theme}>
                 <App {...props} />
-        </ThemeProvider>
+            </ThemeProvider>
         );
     },
     progress: {
-        color: '#4B5563',
+        color: "#4B5563",
     },
 });
