119e479423065d95b1316b3da9afcb7f42d83bd2
Jhunriz <93062821+Jhunriz@users.noreply.github.com>
2023-09-15 09:41:38 +0800

    navbar-active-icon-avatar

--- resources/js/Layouts/MainLayout.jsx
+++ resources/js/Layouts/MainLayout.jsx
@@ -42,6 +42,8 @@ import {
     Collapse,
     Switch,
     Button,
+    Badge,
+    Stack,
 } from "@mui/material";
 import { Logout, PersonAdd, Settings } from "@mui/icons-material";
 
@@ -209,6 +211,43 @@ export default function MainLayout({ user, children }) {
         },
     });
 
+    // active user
+
+    const StyledBadge = styled(Badge)(({ theme }) => ({
+        "& .MuiBadge-badge": {
+            backgroundColor: "#44b700",
+            color: "#44b700",
+            boxShadow: `0 0 0 2px ${theme.palette.background.paper}`,
+            "&::after": {
+                position: "absolute",
+                top: 0,
+                left: 0,
+                width: "100%",
+                height: "100%",
+                borderRadius: "50%",
+                animation: "ripple 1.2s infinite ease-in-out",
+                border: "1px solid currentColor",
+                content: '""',
+            },
+        },
+        "@keyframes ripple": {
+            "0%": {
+                transform: "scale(.8)",
+                opacity: 1,
+            },
+            "100%": {
+                transform: "scale(2.4)",
+                opacity: 0,
+            },
+        },
+    }));
+
+    const SmallAvatar = styled(Avatar)(({ theme }) => ({
+        width: 22,
+        height: 22,
+        border: `2px solid ${theme.palette.background.paper}`,
+    }));
+
     return (
         <ThemeProvider theme={darkTheme}>
             <CssBaseline>
@@ -303,7 +342,24 @@ export default function MainLayout({ user, children }) {
                                 }}
                             >
                                 <MenuItem>
-                                    <Avatar /> {user.name}
+                                    <Stack direction="row" spacing={2}>
+                                        <div className="flex justify-center items-center gap-5">
+                                            <StyledBadge
+                                                overlap="circular"
+                                                anchorOrigin={{
+                                                    vertical: "bottom",
+                                                    horizontal: "right",
+                                                }}
+                                                variant="dot"
+                                            >
+                                                <Avatar
+                                                    alt={user.name}
+                                                    src="/static/images/avatar/1.jpg"
+                                                />
+                                            </StyledBadge>
+                                            {user.name}
+                                        </div>
+                                    </Stack>
                                 </MenuItem>
                                 <MenuItem>
                                     <CustomizedSwitches
