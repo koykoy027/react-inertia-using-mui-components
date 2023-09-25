e1c370d15cadf007dbe329fc23bccab614d13d0b
Jhunriz <93062821+Jhunriz@users.noreply.github.com>
2023-09-25 13:15:23 +0800

    responsive in add-qr-done

--- resources/js/Pages/Admin/Qr/Index.jsx
+++ resources/js/Pages/Admin/Qr/Index.jsx
@@ -104,7 +104,7 @@ export default function Index({ auth, mustVerifyEmail, qrcodes }) {
                     }
                 />
             </div>
-            <div className="py-6">
+            <div className="py-6 overflow-x-auto max-w-[330px] lg:max-h-[100%] lg:max-w-[100%] max-h-[500px] sm:max-h-[auto]">
                 <Card>
                     <MUIDataTable
                         data={data}
