2003d4361393a689a167c89d16906406b2f2b264
Jhunriz <93062821+Jhunriz@users.noreply.github.com>
2023-10-09 13:14:57 +0800

    arrange the equipment page arrange the table

--- resources/js/Pages/Admin/Equipment/Index.jsx
+++ resources/js/Pages/Admin/Equipment/Index.jsx
@@ -1,40 +1,55 @@
 import ShippingCard from "@/Components/Card";
 import MainLayout from "@/Layouts/MainLayout";
 import {
-    Autocomplete,
     Avatar,
-    Button,
+    Box,
     Card,
-    FormControl,
-    InputLabel,
-    MenuItem,
-    Select,
-    TextField,
+    IconButton,
+    ListItem,
+    ListItemAvatar,
+    Menu,
+    Tooltip,
+    Typography,
 } from "@mui/material";
-import React from "react";
+import React, { useState } from "react";
 import MUIDataTable from "mui-datatables";
-import TransitionsModal from "@/Components/Modal";
-import ReusableModal from "@/Components/Modal";
-import AddSharpIcon from "@mui/icons-material/AddSharp";
-import BasicTextFields from "@/Components/InputField";
-import CustomizedSnackbars from "@/Components/CustomizeSnackBar";
 import CustomBreadcrumbs from "@/Components/CustomBreadcrumbs";
 import HomeIcon from "@mui/icons-material/Home";
-import WhatshotIcon from "@mui/icons-material/Whatshot";
-import GrainIcon from "@mui/icons-material/Grain";
 import { Link } from "@inertiajs/react";
 import InventorySharpIcon from "@mui/icons-material/InventorySharp";
 import EastIcon from "@mui/icons-material/East";
+import AddIcon from "@mui/icons-material/Add";
+import SyncAltIcon from "@mui/icons-material/SyncAlt";
+import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
 
 export default function Index({ auth, mustVerifyEmail, status, equipments }) {
+    const [messageEl, setmessageEl] = useState(null);
+
+    const MessagehandleClick = (event) => {
+        setmessageEl(event.currentTarget);
+    };
+
+    const MessagehandleClose = () => {
+        setmessageEl(null);
+    };
     const data = equipments.map((equipments) => [
         equipments.id,
         equipments.equipments,
-        <div className="flex justify-end pr-5">
-            <Link href={route("dashboard")}>
-                <EastIcon />
-            </Link>
-        </div>, // Replace 'insert_icon_name_here' with the actual icon name
+        <div className="flex justify-end pr-5" key={equipments.id}>
+            <Tooltip title="Account settings">
+                <IconButton
+                    onClick={MessagehandleClick} // Make sure you have defined MessagehandleClick function
+                    size="medium"
+                    sx={{ ml: 2 }}
+                    aria-controls={messageEl ? "account-menu" : undefined}
+                    aria-haspopup="true"
+                    aria-expanded={messageEl ? "true" : undefined}
+                >
+                    <MoreVertIcon />
+                </IconButton>
+            </Tooltip>
+        </div>,
     ]);
 
     const columns = ["ID", "Categories", ""];
@@ -77,6 +92,100 @@ export default function Index({ auth, mustVerifyEmail, status, equipments }) {
         <MainLayout user={auth.user}>
             <div className="pb-10">
                 <CustomBreadcrumbs items={breadcrumbItems} />
+            </div>
+            <div>
+                {/* account menu start */}
+                <Box
+                    sx={{
+                        display: "flex",
+                        alignItems: "center",
+                        textAlign: "center",
+                    }}
+                >
+                    <div className="flex justify-center items-center">
+                        <div>
+                            <Menu
+                                id="account-menu"
+                                anchorEl={messageEl}
+                                open={Boolean(messageEl)}
+                                onClose={MessagehandleClose}
+                                PaperProps={{
+                                    elevation: 0,
+                                    sx: {
+                                        overflow: "visible",
+                                        filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.202))",
+                                        mt: 1.5,
+                                        "& .MuiAvatar-root": {
+                                            width: 32,
+                                            height: 32,
+                                            ml: -0.0,
+                                            mr: 2,
+                                        },
+                                        "&:before": {
+                                            content: '""',
+                                            display: "block",
+                                            position: "absolute",
+                                            top: 0,
+                                            right: 14,
+                                            width: 10,
+                                            height: 10,
+                                            bgcolor: "background.paper",
+                                            transform:
+                                                "translateY(-50%) rotate(45deg)",
+                                            zIndex: 0,
+                                        },
+                                    },
+                                }}
+                                transformOrigin={{
+                                    horizontal: "right",
+                                    vertical: "top",
+                                }}
+                                anchorOrigin={{
+                                    horizontal: "right",
+                                    vertical: "bottom",
+                                }}
+                            >
+                                {/* <MessageNotification /> */}
+                                <Link href={route("equipment.create")}>
+                                    <ListItem>
+                                        <ListItemAvatar>
+                                            <Avatar>
+                                                <AddIcon />
+                                            </Avatar>
+                                        </ListItemAvatar>
+                                        <Typography variant="subtitle2">
+                                            Create
+                                        </Typography>
+                                    </ListItem>
+                                </Link>
+                                <Link href={route("branch.create")}>
+                                    <ListItem>
+                                        <ListItemAvatar>
+                                            <Avatar>
+                                                <SyncAltIcon />
+                                            </Avatar>
+                                        </ListItemAvatar>
+                                        <Typography variant="subtitle2">
+                                            Update
+                                        </Typography>
+                                    </ListItem>
+                                </Link>
+                                <Link href={route("branch.create")}>
+                                    <ListItem>
+                                        <ListItemAvatar>
+                                            <Avatar>
+                                                <DeleteForeverIcon />
+                                            </Avatar>
+                                        </ListItemAvatar>
+                                        <Typography variant="subtitle2">
+                                            Delete
+                                        </Typography>
+                                    </ListItem>
+                                </Link>
+                            </Menu>
+                        </div>
+                    </div>
+                </Box>
             </div>
             <div className="py-6 overflow-x-auto max-w-[370px] lg:max-h-[100%] lg:max-w-[100%] max-h-[500px] sm:max-h-[auto]">
                 <div className="sm:hidden">
